15a8f486f0dca4239b895f278d36eb16938fe2f5
nyzs <oknakoca@gmail.com>
2025-02-07 18:09:47 +0100

    init shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ function App() {
 
     return (
         <div>
-            <h1>{count}</h1>
-            <button onClick={() => setCount((prev) => prev++)}>inc</button>
+            <h1 className="text-6xl font-bold">{count}</h1>
+            <button
+                onClick={() => setCount((prev) => prev + 1)}
+                className="rounded-md px-4 py-1.5 bg-amber-500  active:bg-amber-400 transition-colors border-2 border-zinc-400 shadow-sm shadow-black font-medium hover:bg-amber-300"
+            >
+                increment
+            </button>
         </div>
     );
 }
