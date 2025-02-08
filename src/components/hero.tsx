7284b92695fec00a5e0466e0149e192a9828488e
nyzs <oknakoca@gmail.com>
2025-02-08 13:46:30 +0100

    fix: added prevent default on form since its unfinished

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -22,9 +22,10 @@ export default function Hero() {
                             </p>
 
                             <form
-                                action="#"
-                                method="POST"
+                                // action="/"
+                                // method="POST"
                                 className="mt-8 sm:mt-10"
+                                onSubmit={(e) => e.preventDefault()}
                             >
                                 <div className="relative p-2 sm:border sm:border-gray-400 group sm:rounded-xl sm:focus-within:ring-1 sm:focus-within:ring-gray-900 sm:focus-within:border-gray-900">
                                     <input
