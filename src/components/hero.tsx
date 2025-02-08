0fdef65d078610c934b2294304fde2e21ba3bb52
nyzs <oknakoca@gmail.com>
2025-02-08 11:42:08 +0100

    feat: update Hero component with new messaging and improved styling

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,17 +9,21 @@ export default function Hero() {
                 <div className="grid max-w-lg grid-cols-1 mx-auto lg:max-w-full lg:items-center lg:grid-cols-2 gap-y-12 lg:gap-x-16">
                     <div>
                         <div className="text-center lg:text-left">
-                            <h1 className="text-4xl font-bold leading-tight text-foreground sm:text-5xl sm:leading-tight lg:leading-tight lg:text-6xl font-pj">
-                                Lorem ipsum{" "}
+                            <h1 className="text-muted-foreground">
+                                Transform sensor data into predictive insights -
+                                access industry-specific datasets to supercharge
+                                your ML models
+                            </h1>
+                            <h1 className="text-4xl font-bold leading-tight text-foreground sm:text-4xl sm:leading-tight lg:leading-tight lg:text-5xl font-pj">
                                 <span className="text-emerald-500 dark:text-emerald-400">
-                                    dolor
+                                    IoT Intelligence
                                 </span>{" "}
-                                sit amet
+                                Platform with Industry Dataset Marketplace
                             </h1>
                             <p className="mt-2 text-lg text-muted-foreground sm:mt-8 font-inter">
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit. Vehicula massa in enim luctus.
-                                Rutrum arcu.
+                                Transform sensor data into predictive insights -
+                                access industry-specific datasets to supercharge
+                                your ML models
                             </p>
 
                             <form
@@ -33,13 +37,12 @@ export default function Hero() {
                                         name=""
                                         id=""
                                         placeholder="Enter email address"
-                                        className="block w-full px-4 py-4 text-foreground placeholder-foreground bg-transparent border border-accent-foreground outline-none focus:border-background focus:ring-1 focus:ring-background rounded-xl sm:border-none sm:focus:ring-0 sm:focus:border-transparent"
+                                        className="block w-full px-3 py-2 text-foreground placeholder-foreground bg-transparent border border-accent-foreground outline-none focus:border-background focus:ring-1 focus:ring-background rounded-xl sm:border-none sm:focus:ring-0 sm:focus:border-transparent"
                                     />
                                     <div className="mt-4 sm:mt-0 sm:absolute sm:inset-y-0 sm:right-0 sm:flex sm:items-center sm:pr-2">
                                         <Button
                                             type="submit"
-                                            // className="inline-flex px-6 py-3 text-lg font-bold text-white transition-all duration-200 bg-gray-900 rounded-lg focus:outline-none focus:bg-gray-600 font-pj hover:bg-gray-600"
-                                            className="py-6 text-lg"
+                                            className="py-4 text-lg"
                                         >
                                             Subscribe
                                         </Button>
